f1bfb7d0d4d8542628689ae024f44c805c51edd6
EHIREMEN ISAIAH <ehis.isaiah@gmail.com>
2019-03-07 21:42:14 +0100

    The Math Game is now working and has a Get New Card Button

---
+++ output.js
@@ -0,0 +1,23 @@
+function addition(){
+      var yourAnswer = document.getElementById("answer").value;
+	  isNaN(yourAnswer);
+	  if(isNaN(yourAnswer)){
+		   document.getElementById("output").innerHTML = "Hey! " + yourAnswer + " is not a Number. Please enter a Number.";
+	  } else {
+          if ( yourAnswer == correctAnswer){
+              document.getElementById("output").innerHTML = "correct  " + numOne + "  +  " + numTwo + "  =  " +  correctAnswer;
+          } else {
+              document.getElementById("output").innerHTML = "incorrect  " + numOne + "  +  " + numTwo + "  =  " +  correctAnswer + ",   not  " + yourAnswer; 
+          }  
+	  }
+}
+
+function newCard(){
+    document.getElementById("output").innerHTML = "";
+    document.getElementById("answer").value = "";
+    
+     numOne = Math.floor(Math.random()* 10) + 1 ;
+     numTwo =  Math.floor(Math.random()* 10) + 1;
+     document.getElementById("inner_script").innerHTML = numOne + " + " + numTwo;
+     correctAnswer = numOne + numTwo;
+}
